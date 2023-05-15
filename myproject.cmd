c84d256701e3dec9a6878de5926a87673984851a
Chae Won Im <114221089+ChaeWonIm0@users.noreply.github.com>
2023-05-15 12:00:19 +0900

    230515

---
+++ myproject.cmd
@@ -0,0 +1,6 @@
+@echo off
+cd C:\myproject\helloflask
+set FLASK_APP=pybo
+set FLASK_DEBUG=true
+set APP_CONFIG_FILE=C:\myproject\helloflask\myproject\config\development.py
+C:\venvs\myproject\Scripts\activate
